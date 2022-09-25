85d28d2111b4e545437fdc88ca35d98d076b7ab2
zhangruihan1 <47313357+zhangruihan1@users.noreply.github.com>
2022-09-25 21:40:28 +0800

    Update mnist_models.py

--- mnist_models.py
+++ mnist_models.py
@@ -58,7 +58,7 @@ class MLPBN(nn.Module):
 	
 class ConvNet(nn.Module):
 	def __init__(self):
-		super(Net, self).__init__()
+		super(ConvNet, self).__init__()
 		self.conv1 = nn.Conv2d(1, 32, 3, 1)
 		self.conv2 = nn.Conv2d(32, 64, 3, 1)
 		self.dropout1 = nn.Dropout(0.25)
